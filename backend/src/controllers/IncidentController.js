9dbc7e05a46c53db7e064dc5e6d7e39d6f2b3306
Rafael <rafaelrutzsantos@gmail.com>
2020-03-29 19:21:40 -0300

    Mobile App finished

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -14,6 +14,7 @@ module.exports =
             .limit(5)
             .offset((page - 1)*5)
             .select(['incidents.*',
+                     'ongs.name',
                      'ongs.email',
                      'ongs.whatsapp', 
                      'ongs.city',
